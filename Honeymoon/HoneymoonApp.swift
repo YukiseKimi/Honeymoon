a2c081d3791a115e5e28576aa02bfbe028327002
YukiseKimi <mannychau@gmail.com>
2021-10-22 14:59:05 +0100

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Manny Chau on 2021-10-22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
